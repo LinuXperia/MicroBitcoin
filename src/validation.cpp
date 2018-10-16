2b6516c91e7e672950aa1289239f87822e8ae408
itwysgsl <contact@microbitcoin.org>
2018-10-16 20:51:28 +0900

    Block ActivateBestChain to empty validationinterface queue

--- src/validation.cpp
+++ src/validation.cpp
@@ -46,6 +46,7 @@
 #include "warnings.h"
 
 #include <atomic>
+#include <future>
 #include <sstream>
 #include <string>
 
@@ -2601,6 +2602,18 @@ bool CChainState::ActivateBestChain(CValidationState &state, const CChainParams&
 
     do {
         boost::this_thread::interruption_point();
+
+        if (GetMainSignals().CallbacksPending() > 10) {
+            // Block until the validation queue drains. This should largely
+            // never happen in normal operation, however may happen during
+            // reindex, causing memory blowup  if we run too far ahead.
+            std::promise<void> promise;
+            CallFunctionInValidationInterfaceQueue([&promise] {
+                promise.set_value();
+            });
+            promise.get_future().wait();
+        }
+
         if (ShutdownRequested())
             break;
 
