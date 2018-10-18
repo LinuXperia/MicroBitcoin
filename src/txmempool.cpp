90992f82827174c54ecfe4bd4eac0a658ea1c584
itwysgsl <contact@microbitcoin.org>
2018-10-18 14:57:08 +0900

    Fix estimatesmartfee rounding display issue

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -988,8 +988,9 @@ const CTxMemPool::setEntries & CTxMemPool::GetMemPoolChildren(txiter entry) cons
 
 CFeeRate CTxMemPool::GetMinFee(size_t sizelimit) const {
     LOCK(cs);
-    if (!blockSinceLastRollingFeeBump || rollingMinimumFeeRate == 0)
-        return CFeeRate(rollingMinimumFeeRate);
+    if (!blockSinceLastRollingFeeBump || rollingMinimumFeeRate == 0) {
+        return CFeeRate(llround(rollingMinimumFeeRate));
+    }
 
     int64_t time = GetTime();
     if (time > lastRollingFeeUpdate + 10) {
@@ -1007,7 +1008,8 @@ CFeeRate CTxMemPool::GetMinFee(size_t sizelimit) const {
             return CFeeRate(0);
         }
     }
-    return std::max(CFeeRate(rollingMinimumFeeRate), incrementalRelayFee);
+    
+    return std::max(CFeeRate(llround(rollingMinimumFeeRate)), incrementalRelayFee);
 }
 
 void CTxMemPool::trackPackageRemoved(const CFeeRate& rate) {
