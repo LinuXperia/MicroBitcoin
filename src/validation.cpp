a78ac736d5affdc88e6a7c05c783f53bac567e5a
volbil <volbilnexus@gmail.com>
2018-10-18 10:34:58 +0900

    Enable disablesafemode by default

--- src/validation.cpp
+++ src/validation.cpp
@@ -1649,7 +1649,7 @@ static bool WriteUndoDataForBlock(const CBlockUndo& blockundo, CValidationState&
         CDiskBlockPos _pos;
         if (!FindUndoPos(state, pindex->nFile, _pos, ::GetSerializeSize(blockundo, SER_DISK, CLIENT_VERSION) + 40))
             return error("ConnectBlock(): FindUndoPos failed");
-        if (!UndoWriteToDisk(blockundo, _pos, pindex->pprev->GetBlockHash(), chainparams.MessageStart()))
+        if (!UndoWriteToDisk(blockundo, _pos, pindex->pprev->GetBlockHash(), chainparams.BitcoinMessageStart()))
             return AbortNode(state, "Failed to write undo data");
         // update nUndoPos in block index
         pindex->nUndoPos = _pos.nPos;
@@ -3362,7 +3362,7 @@ static CDiskBlockPos SaveBlockToDisk(const CBlock& block, int nHeight, const CCh
         return CDiskBlockPos();
     }
     if (dbp == nullptr) {
-        if (!WriteBlockToDisk(block, blockPos, chainparams.MessageStart())) {
+        if (!WriteBlockToDisk(block, blockPos, chainparams.BitcoinMessageStart())) {
             AbortNode("Failed to write block");
             return CDiskBlockPos();
         }
