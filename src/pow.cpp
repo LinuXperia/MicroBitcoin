343d0b47d03bd8627827f21bc878257c9813131c
itwysgsl <contact@microbitcoin.org>
2018-10-24 18:15:32 +0900

    Updated variable names to camel case

--- src/pow.cpp
+++ src/pow.cpp
@@ -153,43 +153,43 @@ unsigned int Lwma3CalculateNextWorkRequired(const CBlockIndex* pindexLast, const
     const int64_t N = params.lwmaAveragingWindow;
     const int64_t k = N * (N + 1) * T / 2;
     const int64_t height = pindexLast->nHeight;
-    const arith_uint256 pow_limit = UintToArith256(params.powLimitStart);
+    const arith_uint256 powLimit = UintToArith256(params.powLimitStart);
     
-    if (height < N) { return pow_limit.GetCompact(); }
+    if (height < N) { return powLimit.GetCompact(); }
 
-    arith_uint256 sum_target, previous_diff, next_target;
-    int64_t this_timestamp, previous_timestamp;
-    int64_t t = 0, j = 0, solvetime_sum;
+    arith_uint256 sumTarget, previousDiff, nextTarget;
+    int64_t thisTimestamp, previousTimestamp;
+    int64_t t = 0, j = 0, solvetimeSum;
 
-    const CBlockIndex* block_previous_timestamp = pindexLast->GetAncestor(height - N);
-    previous_timestamp = block_previous_timestamp->GetBlockTime();
+    const CBlockIndex* blockPreviousTimestamp = pindexLast->GetAncestor(height - N);
+    previousTimestamp = blockPreviousTimestamp->GetBlockTime();
 
     // Loop through N most recent blocks. 
     for (int64_t i = height - N + 1; i <= height; i++) {
         const CBlockIndex* block = pindexLast->GetAncestor(i);
-        this_timestamp = (block->GetBlockTime() > previous_timestamp) ? block->GetBlockTime() : previous_timestamp + 1;
+        thisTimestamp = (block->GetBlockTime() > previousTimestamp) ? block->GetBlockTime() : previousTimestamp + 1;
 
-        int64_t solvetime = std::min(6 * T, this_timestamp - previous_timestamp);
-        previous_timestamp = this_timestamp;
+        int64_t solvetime = std::min(6 * T, thisTimestamp - previousTimestamp);
+        previousTimestamp = thisTimestamp;
 
         j++;
         t += solvetime * j; // Weighted solvetime sum.
         arith_uint256 target;
         target.SetCompact(block->nBits);
-        sum_target += target / (k * N);
+        sumTarget += target / (k * N);
 
-        if (i > height - 3) { solvetime_sum += solvetime; }
-        if (i == height) { previous_diff = target.SetCompact(block->nBits); }
+        if (i > height - 3) { solvetimeSum += solvetime; }
+        if (i == height) { previousDiff = target.SetCompact(block->nBits); }
     }
 
-    next_target = t * sum_target;
+    nextTarget = t * sumTarget;
     
-    if (next_target > (previous_diff * 150) / 100) { next_target = (previous_diff * 150) / 100; }
-    if ((previous_diff * 67) / 100 > next_target) { next_target = (previous_diff * 67); }
-    if (solvetime_sum < (8 * T) / 10) { next_target = previous_diff * 100 / 106; }
-    if (next_target > pow_limit) { next_target = pow_limit; }
+    if (nextTarget > (previousDiff * 150) / 100) { nextTarget = (previousDiff * 150) / 100; }
+    if ((previousDiff * 67) / 100 > nextTarget) { nextTarget = (previousDiff * 67); }
+    if (solvetimeSum < (8 * T) / 10) { nextTarget = previousDiff * 100 / 106; }
+    if (nextTarget > powLimit) { nextTarget = powLimit; }
 
-    return next_target.GetCompact();
+    return nextTarget.GetCompact();
 }
 
 static unsigned int BitcoinNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHeader *pblock, const Consensus::Params& params)
